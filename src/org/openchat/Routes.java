8ff14abc5dc66728a119143604cad7c0e9f1a130
hernan.wilkinson <hernan.wilkinson@10pines.com>
2020-10-20 15:56:32 -0300

    test de integración de login funcionando

--- src/org/openchat/Routes.java
+++ src/org/openchat/Routes.java
@@ -20,13 +20,21 @@ public class Routes {
     private void openchatRoutes() {
         get("status", (req, res) -> "OpenChat: OK!");
         post("users", (req, res) -> registerUser(req,res));
-        post("login", (req, res) -> "Implementar!");
+        post("login", (req, res) -> login(req,res));
         get("users", (req, res) -> "Implementar!");
         post("users/:userId/timeline", (req, res) -> "Implementar!");
         get("users/:userId/timeline", (req, res) -> "Implementar!");
         post("followings", (req, res) -> "Implementar!");
         get("followings/:followerId/followees", (req, res) -> "Implementar!");
         get("users/:userId/wall", (req, res) -> "Implementar!");
+    }
+
+    private String login(Request request, Response response) {
+        ReceptionistResponse receptionistResponse = receptionist.login(request.body());
+
+        response.status(receptionistResponse.status());
+        response.type("application/json");
+        return receptionistResponse.responseBody();
     }
 
     private String registerUser(Request request, Response response) {
