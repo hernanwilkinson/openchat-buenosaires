692b8cb112159af3887769ca805bc0c393927e23
hernan.wilkinson <hernan.wilkinson@10pines.com>
2020-10-31 18:48:01 -0300

    Test en RestReceptionist de agregar url fallando

--- tests/bsas/org/openchat/TestObjectsBucket.java
+++ tests/bsas/org/openchat/TestObjectsBucket.java
@@ -16,6 +16,7 @@ public class TestObjectsBucket {
     public static final String JUAN_PEREZ_NAME = "Juan Perez";
     public static final String JUAN_PEREZ_PASSWORD = "otherPassword";
     public static final String JUAN_PEREZ_ABOUT = "about";
+    public static final String JUAN_PEREZ_URL = "www.twitter.com/juanPerez";
 
     private final LocalDateTime now = LocalDateTime.now();
 
