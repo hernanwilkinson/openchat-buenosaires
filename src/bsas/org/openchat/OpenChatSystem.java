80b106811f7a83b4270cfe83684f6c1f96c142f3
hernan.wilkinson <hernan.wilkinson@10pines.com>
2020-12-01 13:33:20 -0300

    Mover UserCard a upper level

--- src/bsas/org/openchat/OpenChatSystem.java
+++ src/bsas/org/openchat/OpenChatSystem.java
@@ -124,40 +124,4 @@ public class OpenChatSystem {
         return userCards.values().stream();
     }
 
-    private static class UserCard {
-
-        private final User user;
-        private final String password;
-        private final Publisher publisher;
-
-        public UserCard(User user, String password, Publisher publisher) {
-            this.user = user;
-            this.password = password;
-            this.publisher = publisher;
-        }
-
-        public static UserCard of(User user, String password, Publisher publisher) {
-            return new UserCard(user,password,publisher);
-        }
-
-        public User user() {
-            return user;
-        }
-
-        public boolean isPassword(String potentialPassword) {
-            return password.equals(potentialPassword);
-        }
-
-        public Publisher publisher() {
-            return publisher;
-        }
-
-        public Stream<Publication> publications() {
-            return publisher.publications();
-        }
-
-        public boolean isUserNamed(String potentialUserName) {
-            return user.isNamed(potentialUserName);
-        }
-    }
 }
